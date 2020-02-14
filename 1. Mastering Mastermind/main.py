5ca865522c0dfcfa9e6f3324ce33c94bbe8c2994
Jort <Jortpals@gmail.com>
2020-02-14 12:25:43 +0100

    werkend feedback systeem

--- 1. Mastering Mastermind/main.py
+++ 1. Mastering Mastermind/main.py
@@ -1,48 +1,64 @@
 import random
-colors = ['geel', 'wit', 'zwart', 'groen', 'rood', 'blauw']
+pins = ['A', 'B', 'C', 'D', 'E', 'F']
+
+# Genereerd lijst met alle mogelijke antwoorden
+ans_p = []
+for x_1 in pins:
+    for x_2 in pins:
+        for x_3 in pins:
+            for x_4 in pins:
+                string = x_1 + x_2 + x_3 + x_4
+                ans_p.append(string)
 
 
-def create_code(code):
+def generate_code():
+    secrete_code = []
     for i in range(4):
-        code.append(colors[random.randrange(6)])
-    return code
+        secrete_code.append(pins[random.randrange(6)])
+    return secrete_code
 
 
-def check_code(code, ant, i, secret_code):
-    for x in range(4):
-        if code[i] == secret_code[x]:
-            ant += code[i] + ' : W '
-            return ant
-    ant += code[i] + ' :  '
-    return ant
+def feedback(code, secrete_code):
+    fb = {'Z': 0, 'W': 0}
+    pinz = {'A': [0, 0, 0, 0], 'B': [0, 0, 0, 0], 'C': [0, 0, 0, 0], 'D': [0, 0, 0, 0], 'E': [0, 0, 0, 0], 'F': [0, 0, 0, 0]}
+    for i in range(4):
+        if code[i] == secrete_code[i]:
+            fb['Z'] += 1
+            if pinz[code[i]][i] == 1:
+                fb['W'] -= 1
+            pinz[code[i]][i] = 1
+        else:
+            for x in range(4):
+                if code[i] == secrete_code[x] and pinz[code[i]][x] == 0:
+                    fb['W'] += 1
+                    pinz[code[i]][x] = 1
+                    break
+    return fb
 
 
 def speler():
-    print('Te gebruiken kleuren: geel, wit, zwart, groen, rood, blauw ')
-    secret_code = create_code([])
+    secret_code = generate_code()
     tr = 0
     while tr < 10:
-        ant = ''
-        code = input('Voer vier kleuren in: ').split(' ')
-        if code == secret_code:
-            print('Gelfeliciteerd!, je hebt gewonnen')
-            break
-        else:
-            for i in range(4):
-                if code[i] == secret_code[i]:
-                    ant += code[i] + ' : Z '
-                else:
-                    ant = check_code(code, ant, i, secret_code)
-        tr += 1
+        gues = list(input('Doe een gok: ').upper())
+        ant = feedback(gues, secret_code)
         print(ant)
+        tr += 1
+        if ant['Z'] == 4:
+            print('Gelfeliciteerd je het gewonnen')
+
+    print('Helaas verloren')
 
 
 def mastermind():
-    print('#')
+    secret_code = list(input('Voer vier letters in A t/m F: ').upper())
+    code = list(ans_p[random.randrange(1296)])
+    print(code)
+    print(feedback(code, secret_code))
 
 
-que = input('Spelen als \'Mastermind\' of als \'Speler\': ')
-if que.capitalize() == 'Speler':
+que = input('Spelerm als \'Mastermind\' of \'Speler\': ').capitalize()
+if que == 'Speler':
     speler()
 else:
     mastermind()
