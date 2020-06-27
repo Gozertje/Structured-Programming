ca8055ea39a5878ea292051ba71ab8ee5c56f24e
Jort <Jortpals@gmail.com>
2020-06-27 12:15:36 +0200

    herziene versie

--- 3. Business rules voor Recommendation Engine/recommendation_engine.py
+++ 3. Business rules voor Recommendation Engine/recommendation_engine.py
@@ -2,87 +2,98 @@ import psycopg2
 import csv
 
 # Maakt een conectie met de DB
-table_names = []
 c = psycopg2.connect("dbname=sp user=postgres password=Jopa2002jp")
 cur = c.cursor()
+cur.execute("DROP TABLE IF EXISTS user_rec CASCADE")
+cur.execute("""CREATE TABLE user_rec
+                (id VARCHAR, u1 VARCHAR, u2 VARCHAR, u3 VARCHAR, u4 VARCHAR, u5 VARCHAR, u6 VARCHAR, u7 VARCHAR,
+                u8 VARCHAR, u9 VARCHAR, u10 VARCHAR, u11 VARCHAR, u12 VARCHAR, u13 VARCHAR, u14 VARCHAR, u16 VARCHAR, 
+                u17 VARCHAR, u18 VARCHAR, u19 VARCHAR, u20 VARCHAR, u21 VARCHAR, u22 VARCHAR)""")
 
 
-# Slaat alle tabel namen op, om zo makkelijker de tabellen te kunnen exporteren naar een database
-def write_table_names():
-    with open('csv/#table_names.txt', 'w') as table_names_file:
-        for name in table_names:
-            table_names_file.write(name + '\n')
-        table_names_file.close()
+def create_csv(fieldname, filename):
+    with open('csv/' + filename + '.csv', mode='w', newline="") as csv_file:
+        csv_write = csv.DictWriter(csv_file, fieldnames=fieldname)
+        csv_write.writeheader()
 
 
 # Schrijft alle tabellen naar een csv bestand (wordt opgeslagen onder ../csv/)
-def write_csv(f_name, content, field_name):
-    global table_names
-    table_names.append(f_name)
-    with open('csv/' + f_name + '.csv', mode='w') as csv_file:
-        fieldnames = field_name
-        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        csv_writer.writeheader()
-        for var in content:
-            csv_writer.writerow({field_name[0]: var})
-        csv_file.close()
+def write_csv(filename, line):
+    with open('csv/' + filename + '.csv', mode='a', newline="") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(line)
 
 
-# Sorteert de producten op categorie en doelgroep
 def sort_category():
-    counter = {}
-    cur.execute("""SELECT id, category, targetaudience FROM products""")
-    query_results = cur.fetchall()
-    for product in query_results:
-        product = list(product)
-        if product[1] is not None:
-            if product[2] is None:
-                product[2] = 'All'
-            if (product[1] + product[2][1::2]) in counter:
-                counter[(product[1] + product[2][1::2])] += [product[0]]
-            else:
-                counter[(product[1] + product[2][1::2])] = [product[0]]
-    for category_name in counter:
-        write_csv(category_name, counter[category_name], ['id'])
+    cur.execute("SELECT * FROM products")
+    data = cur.fetchall()
+    dictionary = {}
+    for item in data:
+        for item_2 in data:
+            if item[3: 6] == item_2[3: 6]:
+                if item[0] in dictionary:
+                    dictionary[item[0]] += [item_2[0]]
+                else:
+                    dictionary.update({item[0]: [item[0]]})
+                    dictionary[item[0]] += [item_2[0]]
+        write_csv('products_rec', dictionary[item[0]][0: 23])
+        dictionary.__delitem__(item[0])
+        list.remove(data, item)
 
 
-def BinarySearch(lys, val):
-    first = 0
-    last = len(lys)-1
-    index = -1
-    while (first <= last) and (index == -1):
-        mid = (first+last)//2
-        if lys[mid][0] == val:
-            index = mid
-        else:
-            if val < lys[mid][0]:
-                last = mid - 1
-            else:
-                first = mid + 1
-    return index
+def sort_user():
+    cur.execute("SELECT * FROM profiles_previously_viewed")
+    data = cur.fetchall()
+    dictionary = {}
+    for item in data:
+        for item_2 in data:
+            if item[1] == item_2[1]:
+                if item[1] in dictionary:
+                    dictionary[item[1]] += [item_2[0]]
+                else:
+                    dictionary.update({item[1]: [item[1]]})
+                    dictionary[item[1]] += [item_2[0]]
+                list.remove(data, item_2)
+        if len(dictionary[item[1]]) < 23:
+            for value in range(22 - len(dictionary[item[1]])):
+                dictionary[item[1]] += [None]
+        write_csv('user_rec', dictionary[item[1]][0: 22])
+        dictionary.__delitem__(item[1])
 
 
-def sort_profiles():
-    t = 0
-    counter = {}
-    cur.execute("""SELECT profid, os FROM sessions""")
-    sessions = cur.fetchall()
-    cur.execute("""SELECT profid, prodid FROM profiles_previously_viewed""")
-    ppv = cur.fetchall()
-    for session in sessions:
-        product_i = BinarySearch(ppv, session[0])
-        if session[1] in counter:
-            counter[session[1]] += [ppv[product_i][1]]
-        else:
-            counter[session[1]] = [ppv[product_i][1]]
-        ppv.remove(ppv[product_i])
-        t += 1
-        print(t)
-    for os in counter:
-        write_csv(os, counter[os], ['product'])
+def link_users():
+    cur.execute("""SELECT * FROM user_rec""")
+    data = cur.fetchall()
+    dictionary = {}
+    for item in data:
+        for index in range(1, 22):
+            for sIndex in range(1, 22):
+                if (item[index] != item[sIndex]) and (item[index] is not None) and (item[sIndex] is not None):
+                    if item[index] in dictionary:
+                        if item[sIndex] in dictionary[item[index]]:
+                            dictionary[item[index]][item[sIndex]] += 1
+                        else:
+                            dictionary[item[index]].update({item[sIndex]: 1})
+                    else:
+                        dictionary.update({item[index]: {item[sIndex]: 1}})
+        list.remove(data, item)
+
+    for user in dictionary:
+        for profile in dictionary[user]:
+            write_csv('compatibility', [user, profile, dictionary[user][profile]])
 
 
+create_csv(['id', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13', 'p14', 'p15',
+            'p16', ' p17', 'p18', 'p19', 'p20', 'p21', 'p22'], 'products_rec')
+create_csv(['id', 'u1', 'u2', 'u3', 'u4', 'u5', 'u6', 'u7', 'u8', 'u9', 'u10', 'u11', 'u12', 'u13', 'u14', 'u15',
+            'u16', 'u17', 'u18', 'u19', 'u20', 'u21', 'u22'], 'user_rec')
+create_csv(['id', 'profile', 'compatibility'], 'compatibility')
 sort_category()
-write_table_names()
-sort_profiles()
+sort_user()
 
+with open('csv/user_rec.csv') as csvfile:
+    print("Copying {}...".format('user_rec'))
+    cur.copy_expert("COPY " + "user_rec" + " FROM STDIN DELIMITER ',' CSV HEADER", csvfile)
+    c.commit()
+
+link_users()
